b341677d05275b53256a8a151efb07e009049bc4
uile-alt <haotian908@gmail.com>
2020-08-19 03:07:05 +0800

    init

---
+++ public/main.js
@@ -0,0 +1,98 @@
+let n = 1;
+getPage.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", `/page${n + 1}`);
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        console.log(request.response);
+        const array = JSON.parse(request.response);
+        array.forEach((item) => {
+          const li = document.createElement("li");
+          li.textContent = item.id;
+          xxx.appendChild(li);
+        });
+        n += 1;
+      }
+    }
+  };
+  request.send();
+};
+getCSS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/style.css");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        console.log(request.response);
+        const style = document.createElement("style");
+        style.innerHTML = request.response;
+        document.head.appendChild(style);
+      }
+    }
+  };
+
+  request.send();
+};
+
+getJS.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/2.js");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        console.log(request.response);
+        const script = document.createElement("script");
+        script.innerHTML = request.response;
+        document.body.appendChild(script);
+      }
+    }
+  };
+  request.send();
+};
+getHTML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/3.html");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        console.log(request.response);
+        const div = document.createElement("html");
+        div.innerHTML = request.response;
+        inser.appendChild(div);
+      }
+    }
+  };
+  request.send();
+};
+
+getXML.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "4.xml");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        console.log(request.responseXML);
+        const dom = request.responseXML;
+        const text = dom.getElementsByTagName("warning")[0].textContent;
+        console.log(text.trim());
+      }
+    }
+  };
+  request.send();
+};
+getJSON.onclick = () => {
+  const request = new XMLHttpRequest();
+  request.open("GET", "/5.json");
+  request.onreadystatechange = () => {
+    if (request.readyState === 4) {
+      if (request.status >= 200 && request.status < 300) {
+        console.log(request.response);
+        const object = JSON.parse(request.response);
+        console.log(object);
+        myName.textContent = object.name;
+      }
+    }
+  };
+  request.send();
+};
